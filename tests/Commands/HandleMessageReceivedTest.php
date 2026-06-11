b23cd12722436f753d131eb81e8eab06acde96f5
Steve Bauman <steven_bauman@outlook.com>
2026-06-11 15:50:29 -0400

    Remove no longer necessary test

--- tests/Commands/HandleMessageReceivedTest.php
+++ tests/Commands/HandleMessageReceivedTest.php
@@ -6,24 +6,6 @@ use DirectoryTree\ImapEngine\Laravel\Events\MessageReceived;
 use DirectoryTree\ImapEngine\Testing\FakeMessage;
 use Illuminate\Support\Facades\Event;
 
-it('dispatches event', function () {
-    $command = mock(WatchMailbox::class);
-
-    $command->shouldReceive('info')->once()->with(
-        'Message received: [123]'
-    );
-
-    Event::fake();
-
-    $handle = new HandleMessageReceived($command);
-
-    $handle(new FakeMessage(123));
-
-    Event::assertDispatched(
-        fn (MessageReceived $event) => is_null($event->mailbox)
-    );
-});
-
 it('dispatches event with mailbox name', function () {
     $command = mock(WatchMailbox::class);
 
